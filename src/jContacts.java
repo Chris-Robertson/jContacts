54e460f8c608f9d53529042d66992b74a16c747a
Chris Robertson <chrisxrobertson@gmail.com>
2017-08-17 20:11:03 +1000

    Initial commit.

---
+++ src/jContacts.java
@@ -0,0 +1,31 @@
+// Write a java program that allows a user to add multiple contacts (each consisting of a name and a phone number), and
+// can print out all the contacts.  Persistence is not required, storing contacts in memory only during the execution of
+// the program is fine.
+
+// create a list of contacts
+
+// loop through a console app
+
+// display a menu to the user
+// - Add Contact
+// - Display Contacts
+// - Quit
+
+// Add Contact
+// Ask user for contact name
+// Check input
+// Store name in contacts list
+// Ask user for phone number
+// Check input
+// Store phone number in contacts list
+
+// loop
+
+public class jContacts {
+
+    public static void main(String... args) {
+
+
+
+    }
+}
